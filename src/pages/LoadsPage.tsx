e145fe8052876d6f855f938e98f0a0ad59fe6da1
babkenpokrikyan <babken@benekiva.com>
2025-11-02 01:21:23 +0400

    Added Load managment icon

--- src/pages/LoadsPage.tsx
+++ src/pages/LoadsPage.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Flex, Typography } from 'antd'
 import { LoadManagementTable } from '@/features/load-management'
+import { BankOutlined, TruckOutlined } from "@ant-design/icons";
 
 const { Title } = Typography
 
 const LoadsPage: React.FC = () => {
   return (
     <Flex vertical gap={24}>
-      <Title level={2} >Loads Management</Title>
+      <Title level={2} >
+          <TruckOutlined style={{ marginRight: 8 }} />
+          Loads Management
+      </Title>
       <LoadManagementTable />
     </Flex>
   )
